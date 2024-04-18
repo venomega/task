278b071464a2c15f93a25e905c269d189cafa4b3
venomega <guilleps92@gmail.com>
2024-04-17 23:52:48 -0400

    added initial webdav, get working

---
+++ task/webdav.py
@@ -0,0 +1,55 @@
+import socket, ssl, json, os
+
+# missing load config
+
+class Webdav():
+    @classmethod
+    def parse(self, configpath):
+        self = self()
+        d = json.load(open(configpath))
+        if d['username'] == "" or d["password"] == "":
+            self.auth = False
+        else:
+            self.auth = True
+            self.base64encoded = ssl.base64.b64encode(":".join([d['username'], d["password"]]).encode())
+        self.hostname = d["hostname"]
+        self.port = d["port"]
+        self.ssl = d["ssl"]
+        return self
+    
+    def __exec(self):
+        s = socket.socket()
+        if ssl:
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.hostname)
+        try:
+            s.connect((self.hostname, self.port))
+        except:
+            raise("Error Connecting")
+            print("THIS IS NOT SUPPOSED TO BE REACHED")
+        if self.sendall: # uploading file
+            s.sendall(self.request + self.content)
+        else: # reading file
+            s.send(self.request)
+        response = b""
+        while True:
+            data = s.recv(3333)
+            if not data:
+                break
+            response += data
+        print (b" ".join(response.split(b"\r\n")[0].split(b" ")[2:]))
+        n = response.index(b"\r\n\r\n")
+        open("dump", "wb").write(response[n+4:])
+        
+
+    def get(self, filepath, outputfilepath="dump"):
+        self.outputfilepath = outputfilepath
+        if not "/" in filepath[0]:
+            filepath = "/" + filepath
+        if self.auth:
+            self.request = b"GET %b HTTP/1.1\r\nAuthorization: Basic %b\r\nConnection: Close\r\nHost: %b:%i\r\n\r\n" % (filepath.encode(), self.base64encoded, self.hostname.encode(), self.port)
+        else:
+            self.request = b"GET %b HTTP/1.1\r\nConnection: Close\r\nHost: %b:%i\r\n\r\n" % (filepath, self.hostname.encode(), self.port)
+        self.sendall = False
+        self.__exec()
+
