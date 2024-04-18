38ba278895041ea0cc972435bbe1a1680f0fc797
venomega <guilleps92@gmail.com>
2024-04-18 01:01:06 -0400

    - fixed ssl with python latest ctx issue  - added put working

--- task/webdav.py
+++ task/webdav.py
@@ -20,7 +20,7 @@ class Webdav():
     def __exec(self):
         s = socket.socket()
         if ssl:
-            ctx = ssl.create_default_context()
+            ctx = ssl.create_default_context()  # python latest versions
             s = ctx.wrap_socket(s, server_hostname=self.hostname)
         try:
             s.connect((self.hostname, self.port))
@@ -28,6 +28,7 @@ class Webdav():
             raise("Error Connecting")
             print("THIS IS NOT SUPPOSED TO BE REACHED")
         if self.sendall: # uploading file
+            print("Reached", self.request)
             s.sendall(self.request + self.content)
         else: # reading file
             s.send(self.request)
@@ -37,19 +38,35 @@ class Webdav():
             if not data:
                 break
             response += data
-        print (b" ".join(response.split(b"\r\n")[0].split(b" ")[2:]))
-        n = response.index(b"\r\n\r\n")
-        open("dump", "wb").write(response[n+4:])
+        print (b" ".join(response.split(b"\r\n")[0].split(b" ")[2:]).decode())
+        if not self.sendall:
+            n = response.index(b"\r\n\r\n")
+            open("dump", "wb").write(response[n+4:])
         
 
     def get(self, filepath, outputfilepath="dump"):
         self.outputfilepath = outputfilepath
-        if not "/" in filepath[0]:
+        if "/" != filepath[0]:
             filepath = "/" + filepath
         if self.auth:
             self.request = b"GET %b HTTP/1.1\r\nAuthorization: Basic %b\r\nConnection: Close\r\nHost: %b:%i\r\n\r\n" % (filepath.encode(), self.base64encoded, self.hostname.encode(), self.port)
         else:
             self.request = b"GET %b HTTP/1.1\r\nConnection: Close\r\nHost: %b:%i\r\n\r\n" % (filepath, self.hostname.encode(), self.port)
         self.sendall = False
+        self.__exec()
+
+    def put(self, filepath, outputfiledir="/"):   # INCOMPLETO
+        if  "/" != outputfiledir[0]:
+            outputfiledir = "/" + outputfiledir
+        if  "/" != outputfiledir[-1]:
+            outputfiledir = outputfiledir + "/"
+        self.outputfiledir = outputfiledir
+        print(self.outputfiledir)
+        self.content = open(filepath, "rb").read()
+        if self.auth:
+            self.request = b"PUT %b HTTP/1.1\r\nAuthorization: Basic %b\r\nContent-Length: %i\r\nConnection: Close\r\nHost: %b:%i\r\n\r\n" % ((self.outputfiledir + filepath).encode(), self.base64encoded, len(self.content), self.hostname.encode(), self.port)
+        else:
+            self.request = b"PUT %b HTTP/1.1\r\nConnection: Close\r\nContent-Length: %i\r\nHost: %b:%i\r\n\r\n" % ((self.outputfiledir + filepath).encode(), len(self.content), self.hostname.encode(), self.port)
+        self.sendall = True
         self.__exec()
 
