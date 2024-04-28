9a5d921d9af7b59e532316354b883cb769c53948
venomega <guilleps92@gmail.com>
2024-04-28 11:10:04 -0400

    added --time in add, in order to parse task timestamp in future added agenda view

--- task/__main__.py
+++ task/__main__.py
@@ -4,6 +4,7 @@ import os
 import time
 import random
 import webdav
+from datetime import datetime
 
 
 path = os.path.join(os.environ['HOME'], ".task.json")
@@ -57,8 +58,17 @@ def show():
 def add(*args):
     global l
     id = random.randint(100000,999999)
-    timestamp = time.time()
-    text = " ".join(*args)
+    timestamp = 0
+    arg_list = args[0]
+    args_list = args[0]
+    for arg in arg_list:
+        if "--time=" in arg:
+            st = arg.replace("--time=", "")
+            timestamp = int(os.popen(f"date --date='{st}' +%s").read())
+            args_list.remove(arg)
+    if timestamp == 0:
+        timestamp = time.time()
+    text = " ".join(args_list)
     status = 'undone'
     l.append([id, timestamp, status, text])
 
@@ -95,6 +105,71 @@ def sync():
     wv.put(path)
     return 0
 
+def agenda():
+    def parse_missed(element_list):
+        for element in element_list:
+            t = element[1]
+            tt = time.time()
+            text = element[-1]
+            id = element[0]
+            token = (tt - t) // (3600 * 24)
+            if not token > 1:
+                token = str((tt - t) // (3600)) + "h"
+            else:
+                token = str(token) + "d"
+            yield f"[{id}] {token} {text}"
+
+    def parse_today(element_list):
+        for element in element_list:
+            text = element[-1]
+            id = element[0]
+            yield f"[{id}] {text}"
+
+    def parse_week(element_list):
+        for element in element_list:
+            t = element[1]
+            text = element[-1]
+            id = element[0]
+            token = datetime.fromtimestamp(t)
+            yield f"[{id}] {token.strftime("%d/%m")} {text}"
+
+    global l
+    token = l
+    token.sort(key=lambda x: x[1])
+    start = datetime.now()
+    start = start.replace(hour=0, minute=0, second=0, microsecond=0)
+    end = start.replace(hour=23, minute=59, second=59, microsecond=0)
+    week = time.time() + (3600 * 24 * 7)
+    month = week + (3600 * 24 * 7 * 4)
+    missed = []
+    today = []
+    within_next_week = []
+    within_next_month = []
+    for element in token:
+        if element[1] < start.timestamp() and element[-2] == "undone":
+            missed.append(element)
+        elif element[1] >= start.timestamp() and element[1] <= end.timestamp() and element[-2] == "undone":
+            today.append(element)
+        elif element[1] > end.timestamp() and element[1] <= week and element[-2] == "undone":
+            within_next_week.append(element)
+        elif element[1] > week and element[1] <= month and element[-2] == "undone":
+            within_next_month.append(element)
+        else:
+            pass
+    if len(missed) != 0:
+        print("Missed")
+        print("\n".join(parse_missed(missed)))
+    if len(today) != 0:
+        print("Today", datetime.now().strftime("%d/%m"))
+        print("\n".join(parse_today(today)))
+    if len(within_next_week) != 0:
+        print("Next Week")
+        print("\n".join(parse_week(within_next_week)))
+    if len(within_next_month) != 0:
+        print("Next Month")
+        print("\n".join(parse_week(within_next_month)))
+    return 0
+
 def main():
     load()
     if len(sys.argv) < 2:
@@ -110,6 +185,8 @@ def main():
         done(int(sys.argv[2]))
     elif sys.argv[1] == "sync":
         sync()
+    elif sys.argv[1] == "agenda":
+        agenda()
     save()
     return 0
 
